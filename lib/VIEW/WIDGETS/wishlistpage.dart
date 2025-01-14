45260d50580448d8a3a5b169cd7bbc2556cb9f68
VinujaVijayan <vinujavijayan99@gmail.com>
2025-01-14 23:53:52 +0530

    commit

--- lib/VIEW/WIDGETS/wishlistpage.dart
+++ lib/VIEW/WIDGETS/wishlistpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_recipe_app/CONTROLLER/wishlistcontrol.dart';
+import 'package:food_recipe_app/VIEW/detailspage.dart';
 import 'package:get/get.dart';
 
 class WishlistPage extends StatelessWidget {
@@ -22,19 +23,24 @@ class WishlistPage extends StatelessWidget {
           itemCount: wishcontrol.wishList.length,
           itemBuilder: (context, index) {
             final food = wishcontrol.wishList[index];
-            return ListTile(
-              leading: Image(
-                image: food.image ?? AssetImage('assets/default_image.jpg'),
-                height: 50,
-                width: 50,
-              ),
-              title: Text(food.name ?? ''),
-              subtitle: Text(food.category ?? ''),
-              trailing: IconButton(
-                icon: Icon(Icons.delete),
-                onPressed: () {
-                  wishcontrol.removefromwishList(food);
-                },
+            return InkWell(
+              onTap: () {
+                Get.to(() => DetailsScreen(foood: food));
+              },
+              child: ListTile(
+                leading: Image(
+                  image: food.image ?? AssetImage('assets/default_image.jpg'),
+                  height: 50,
+                  width: 50,
+                ),
+                title: Text(food.name ?? ''),
+                subtitle: Text(food.category ?? ''),
+                trailing: IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    wishcontrol.removefromwishList(food);
+                  },
+                ),
               ),
             );
           },
